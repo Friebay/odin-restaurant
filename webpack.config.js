7b16e440ae3d4de4ff54e8ffbf4d2583eb66ddcd
Friebay <frybaylt@gmail.com>
2024-11-12 15:37:32 +0200

    working very basic

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,24 @@
-// webpack.config.js
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  devtool: "eval-source-map",
   devServer: {
-    watchFiles: ["./src/template.html"],
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    open: true,
+    hot: true,
   },
+  mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
+      template: path.join(__dirname, 'src', 'template.html'),
+      filename: 'index.html',
     }),
   ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
 };
